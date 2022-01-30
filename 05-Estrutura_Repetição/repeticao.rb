5797fc58c8f62f9c6cc26484757649edcd720639
Mateus Santos <ffrmateus@gmail.com>
2022-01-30 15:00:27 -0300

    Estrutura de Repetição While & Each - Ruby

---
+++ 05-Estrutura_Repetição/repeticao.rb
@@ -0,0 +1,16 @@
+# Each
+
+['A','B','C'].each do |i|
+   puts "O valor lido foi: " + i.to_s
+end
+
+
+# While
+
+i = 0
+num = 5
+
+while i <= num  do
+   puts "Contando... " + i.to_s
+   i += 1
+end
