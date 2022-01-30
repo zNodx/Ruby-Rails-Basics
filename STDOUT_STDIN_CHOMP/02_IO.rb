df8f58413b4a74da55a31093d0800d30e86e6f97
Mateus Santos <ffrmateus@gmail.com>
2022-01-30 14:30:52 -0300

    Usando CAST para alterar o tipo da variavel

--- STDOUT_STDIN_CHOMP/02_IO.rb
+++ STDOUT_STDIN_CHOMP/02_IO.rb
@@ -1,7 +1,16 @@
 puts "Digite seu nome:"
 nome = gets.chomp
+
 puts "O seu nome é:" + nome
+
 
 puts "================================"
 
-puts nome.inspect
+puts "com inspect >>>" + nome.inspect
+
+puts "================================"
+
+puts "Digite seu salário:"
+sol = gets.chomp.to_f
+
+puts "Seu salário atualizado é:" + (sol*1.10).to_s
